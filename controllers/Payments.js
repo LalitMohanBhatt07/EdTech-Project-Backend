22e6b3a443ce83afbd41b6685667e243be50a15c
Lb181808 <lb181808@gmail.com>
2024-07-19 18:38:45 +0530

    Wrote handler function for verifying signature and performing action such as adding user to the course model and adding course to the user model and give user access to the course

--- controllers/Payments.js
+++ controllers/Payments.js
@@ -104,5 +104,91 @@ exports.capturePayment=async(req,res)=>{
 //verify signature of razorpay and server
 
 exports.verifySignature=async(req,res)=>{
+    //! we will compare the secret inside server and the secret send by razorpay with the help of webhook
+    const webhookSecret="12345678"
+    
+    const signature=req.headers("x-razorpay-signature")
+
+    const shasum=crypto.createHmac("sha256",webhookSecret) //hmac means hashed based message authentication code. SHA (Secure Hasing algorithm ki help se hum bina kisi ki help se hasing kar sakte h par hmac is the combination of hashing algorithm and secret key). SHA does not use secret key whearas HMAC use secret key. Both SHA and HMAC are used for intigrity authenticity.
+
+    //! razorpay will return us the secret from req.headers("x-razoray-signature") in the form of hashed
+
+    //! encrypt karna possible h par decrypt karna possible nahi h to hum jo hamare server ke pass jo webhookSecret h ose decrypt karna hoga with the help of three steps 1. crypto.createHmac() 2. update to string 3. digest
+
+    //convert shasum to string
+    shasum.update(JSON.stringify(req.body))
+
+    const digest=shasum.digest("hex")
+
+    //compare digest and signature which came from razorpay
+
+    if(signature===digest){
+        console.log("Payment is Authorized")
+        //! ab user authorize ho chuka h to user ko course access do .. User ke course ke andar objectId do and course ke studentEnrolled ke andar userId do
+
+        //get courseId , userId form notes
+    
+        const {courseId,userId}=req.body.payload.entity.notes;
+
+        try{
+        //fullfil the action
+
+        //find the course and enroll teh student in it
+        const enrolledCourse=await Course.findOneAndUpdate({_id:courseId},{$push:{studentsEnrolled:userId}}
+            ,{new:true}
+        )
+
+        if(!enrolledCourse){
+            return res.status(500).json({
+                success:false,
+                message:"Course not found"
+            })
+        }
+
+        console.log(enrolledCourse)
+
+        //find the student and add the course to their list of enrolled courses
+        const enrolledStudent=await User.findOneAndUpdate({_id:userId},{
+            $push:{
+                courses:courseId
+            }
+        },{new:true})
+
+        console.log(enrolledStudent)
+
+        //confirmation mail send kar do use mailSender
+        const emailResponse=await mailSender(
+            enrolledStudent.email,
+            "Congratulations from CodeHelp",
+            "Congratulation, you are onborded into new codehelp course"
+        )
+
+        console.log(emailResponse)
+        return res.status(200).json({
+            success:true,
+            message:"Signature verified and course added"
+        })
+
+        }
+        catch(err){
+        console.error(err)
+        return res.status(500).json({
+            success:false,
+            message:err.message
+        })
+    }
+    }
+
+    else{
+        return res.status(400).json({
+            success:false,
+            message:"Signature does not match"
+        })
+    }
     
 }
+
+
+
+
+
