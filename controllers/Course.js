3478d7b4ff27bae3fee91a26c118e71cfe2d3f6b
Lb181808 <lb181808@gmail.com>
2024-07-21 21:54:08 +0530

    Created Handler function for Edit course

--- controllers/Course.js
+++ controllers/Course.js
@@ -3,6 +3,7 @@ const Category=require("../models/Category")
 const User = require("../models/User")
 const user=require("../models/User")
 const {uploadImageToClodinary}=require("../utils/imageUploder")
+require("dotenv").config()
 
 //create Course handler function
 exports.createCourse=async(req,res)=>{
@@ -158,6 +159,75 @@ exports.getCourseDetails=async(req,res)=>{
         return res.status(500).json({
             success:false,
             message:err.message
+        })
+    }
+}
+
+exports.editCourse=async(req,res)=>{
+    try{
+        const {courseId}=req.body
+        const updates=req.body
+        const course=await Course.findById(courseId)
+
+        if(!course){
+            return res.status(404).json({
+                success:false,
+                message:"course does not found!",
+            })
+        }
+
+        //if thumbnail image is found update it
+        if(req.files){
+            console.log("thumbnail update")
+            const thumbnail=req.files.thumbnailImage
+            const thumbnailImage=await uploadImageToClodinary(thumbnail,process.env.FOLDER_NAME)
+            course.thumbnail=thumbnailImage.secure_url
+        }
+
+        //update only the fields that are present in the request body
+        for(const key in updates){
+            if(updates.hasOwnProperty(key)){
+                if(key=="category"||key=="instructions"){
+                    course[key]==JSON.parse(updates[key])
+                } else{
+                    course[key]=update[key]
+                }
+            }
+        }
+
+        await course.save()
+
+        const updatedCourse=await Course.findOne({
+            _id:courseId
+        })
+        .populate({
+            path:"instructor",
+            populate:{
+                path:"additionalDetails"
+            }
+        })
+        .populate("category")
+        .populate("ratingAndReviews")
+        .populate({
+            path:"courseContent",
+            populate:{
+                path:"subSection"
+            }
+        })
+        .exec()
+
+        res.status(200).json({
+            success:true,
+            message:"Course updated successfully",
+            data:updatedCourse
+        })
+    }
+    catch(err){
+        console.error(err)
+        res.status(500).json({
+            success:false,
+            message:"Cannot edit course",
+            error:err.message
         })
     }
 }
--- routes/Course.js
+++ routes/Course.js
@@ -1,47 +1,57 @@
-// const express=require("express")
-// const router=express.Router()
+const express=require("express")
+const router=express.Router()
 
-// //todo: IMPORTING THE CONTROLLERS
+//todo: IMPORTING THE CONTROLLERS
  
-// //Import Course controller
-// const {
-//     createCourse,
-//     getAllCourses,
-//     getCourseDetails
-// }=require("../controllers/Course")
+//Import Course controller
+const {
+    createCourse,
+    getAllCourses,
+    getCourseDetails
+}=require("../controllers/Course")
 
-// //import Category controller
-// const {
-//     createCategory,
-//     showAllCategories,
-//     categoryPageDetails
-// }=require("../controllers/Category")
+//import Category controller
+const {
+    createCategory,
+    showAllCategories,
+    categoryPageDetails
+}=require("../controllers/Category")
 
-// //import Section Controller
-// const {
-//     createSection,
-//     updateSection,
-//     deleteSection
-// }=require("../controllers/Section")
+//import Section Controller
+const {
+    createSection,
+    updateSection,
+    deleteSection
+}=require("../controllers/Section")
 
-// //import Subsection controller
-// const {
-//     createSubSection,
-//     updateSubSection,
-//     deleteSubSection
-// }=require("../controllers/Subsection")
+//import Subsection controller
+const {
+    createSubSection,
+    updateSubSection,
+    deleteSubSection
+}=require("../controllers/Subsection")
 
-// //importing Rating controller
-// const {
-//     createRating,
-//     getAverageRating,
-//     getAllRatingAndReviews
-// }=require("../controllers/RatingAndReview")
+//importing Rating controller
+const {
+    createRating,
+    getAverageRating,
+    getAllRatingAndReviews
+}=require("../controllers/RatingAndReview")
 
-// //importing middlewares : 
-// const {
-//     auth,
-//     isStudent,
-//     isInstructor,
-//     isAdmin
-// }=require("../middlewares/auth")
+//importing middlewares : 
+const {
+    auth,
+    isStudent,
+    isInstructor,
+    isAdmin
+}=require("../middlewares/auth")
+
+const {updatedCourseProgress}=require("../controllers/courseProgress")
+
+
+// ********************************************************************************************************
+//                                      Course routes
+// ********************************************************************************************************
+
+//! Courses can only be created by Instructors
+router.post("/createCourse",auth,isInstructor,createCourse)
