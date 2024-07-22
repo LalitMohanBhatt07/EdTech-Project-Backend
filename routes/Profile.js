3e44e912a4b8060179d50ec800ef1bb1d31d5cc0
Lb181808 <lb181808@gmail.com>
2024-07-22 13:33:14 +0530

    Created all Routes realted to Profile

--- routes/Profile.js
+++ routes/Profile.js
@@ -1,8 +1,16 @@
-// const express=require("express")
-// const router=express.Router()
+const express=require("express")
+const router=express.Router()
 
-// const {
-//     deleteAccount,
-//     updateProfile,
-//     getAllUserDetail
-// }
+const {
+    deleteAccount,
+    updateProfile,
+    getAllUserDetail,
+    getEnrolledCourses,
+    updateDisplayPicture
+} =require("../controllers/Profile")
+
+router.delete("/deleteProfile",deleteAccount)
+router.put("/updateProfile",auth,updateProfile)
+router.get("/getUserDetails",auth,getAllUserDetail)
+router.get("/getEnrolledCourses",auth,getEnrolledCourses)
+router.put("/updateDisplayPicture",auth,updateDisplayPicture)
